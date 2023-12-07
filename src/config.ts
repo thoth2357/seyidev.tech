d9a5f95db2bd58c1db02af6035ce59a0b8a85252
thoth2357 <thoth2357@gmail.com>
2023-12-07 20:09:45 +0100

    :mag: Improve seo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SeyiDev.Tech';
-export const SITE_DESCRIPTION = 'Portfolio Website for Oluwaseyi Oyewunmi Built with Astro';
+export const SITE_TITLE = 'Home';
+export const SITE_DESCRIPTION = 'Software sorcerer, skilled in the esoteric disciplines of backend engineering, DevOps, and machine learning';

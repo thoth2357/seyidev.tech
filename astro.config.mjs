a3795dae19fe48954135000156eeb886c94bd1f5
thoth2357 <thoth2357@gmail.com>
2023-12-08 15:07:25 +0100

    :heavy_plus_sign: Added astro robot dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://seyidev.tech',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()]
 });

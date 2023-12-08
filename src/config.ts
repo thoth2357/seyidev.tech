4283e8a512615422c157b952b2a59fd462e2ad47
thoth2357 <thoth2357@gmail.com>
2023-12-08 15:08:00 +0100

    :mag: changed site description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Home';
-export const SITE_DESCRIPTION = 'Software sorcerer, skilled in the esoteric disciplines of backend engineering, DevOps, and machine learning';
+export const SITE_DESCRIPTION = 'Software sorcerer/Engineer, skilled in the esoteric disciplines of backend engineering, DevOps, and machine learning';

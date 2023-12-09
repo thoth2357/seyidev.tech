67a7dcce5cb6f376bb2fc6bf10ba4b42121fdf10
thoth2357 <thoth2357@gmail.com>
2023-12-09 12:11:28 +0100

    :package: Added swup package to increase page performance and add animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,29 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 import robotsTxt from "astro-robots-txt";
+import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://seyidev.tech',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()]
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(),
+    swup({
+      theme: 'fade',
+      animationClass: 'transition-',
+      cache: true,
+      preload: true,
+      accessibility: true,
+      forms: false,
+      morph: false,
+      parallel: false,
+      progress: false,
+      routes: false,
+      smoothScrolling: true,
+      updateBodyClass: false,
+      updateHead: true,
+      reloadScripts: true,
+      debug: false,
+      loadOnIdle: true,
+      globalInstance: false,
+    })]
 });

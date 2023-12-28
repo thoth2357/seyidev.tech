e3ba769ef59b0714948553fee1a6a55f752fbfce
thoth2357 <thoth2357@gmail.com>
2023-12-28 02:30:34 +0100

    :mag: changed site title and desc

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Home';
-export const SITE_DESCRIPTION = 'Software sorcerer/Engineer, skilled in the esoteric disciplines of backend engineering, DevOps, and machine learning';
+export const SITE_TITLE = 'Home 🏡';
+export const SITE_DESCRIPTION = 'Software professional from Lagos, Nigeria with over 7 years of experience developing software solutions. With a deep passion for ML/AI & Backend Engineering, I craft intricate digital solutions using multiple technologies.';
